8acef8be386bab14f3f27bea3da488339bcb680c
Christian Lascsak <christian.lascsak7@gmail.com>
2020-11-28 22:03:46 +0100

    Add StringDistanceResolver and Matrix model

---
+++ src/Helpers/Matrix.h
@@ -0,0 +1,48 @@
+class Matrix
+{
+private:
+    int _rows;
+    int _cols;
+    int **_matrix;
+
+public:
+    Matrix(int rows, int cols);
+    ~Matrix();
+
+    int cols() const;
+    int rows() const;
+    const int **matrix() const;
+};
+
+Matrix::Matrix(int rows, int cols)
+{
+    this->_cols = cols;
+    this->_rows = rows;
+    _matrix = new int *[_cols];
+    for (int i = 0; i < _cols; i++)
+    {
+        _matrix[i] = new int[_rows];
+    }
+}
+
+Matrix::~Matrix()
+{
+    for (int i = 0; i < _rows; ++i)
+        delete[] _matrix[i];
+    delete[] _matrix;
+}
+
+int Matrix::rows() const
+{
+    return this->_rows;
+}
+
+int Matrix::cols() const
+{
+    return this->_cols;
+}
+
+const int **Matrix::matrix() const
+{
+    return _matrix;
+}
---
+++ src/Services/StringDistanceResolver.h
@@ -0,0 +1,26 @@
+#include <string>
+#include "Helpers/Matrix.h"
+using string = std::string;
+
+class StringDistanceResolver
+{
+private:
+    /* data */
+public:
+    StringDistanceResolver(/* args */);
+
+    int get_leventshtein_distance(string, string);
+};
+
+StringDistanceResolver::StringDistanceResolver(/* args */)
+{
+}
+
+int StringDistanceResolver::get_leventshtein_distance(string str_l, string str_r)
+{
+    auto cols = str_l.length();
+    auto rows = str_r.length();
+    auto matrix = Matrix(rows, cols);
+
+    return -1;
+}
