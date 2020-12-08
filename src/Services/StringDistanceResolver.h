180297ee6cc551228a2d8f567c82023cd5ce85bf
Christian Lascsak <christian.lascsak7@gmail.com>
2020-12-08 21:05:57 +0100

    refactor get_min and add to two vector function

--- src/Services/StringDistanceResolver.h
+++ src/Services/StringDistanceResolver.h
@@ -98,64 +98,64 @@ int StringDistanceResolver::get_distance_matrix(string str_l, string str_r)
     return matrix[rows - 1][cols - 1];
 }
 
+int StringDistanceResolver::get_distance_two_vec(string str_l, string str_r)
+{
+    auto rows = str_l.length() + 1;
+    auto cols = str_r.length() + 1;
+    auto distance = 0;
+
+    std::vector<int> v0 = std::vector<int>(rows);
+    std::vector<int> v1 = std::vector<int>(cols);
+
+    // initialize v0 (the previous row of distances)
+    // this row is A[0][i]: edit distance for an empty s
+    // the distance is just the number of characters to delete from t
+    for (int i = 0; i < rows; i++)
+    {
+        v0[i] = i;
+    }
+
+    for (int i = 0; i < cols - 1; i++)
+    {
+        // calculate v1 (current row distances) from the previous row v0
+
+        // first element of v1 is A[i+1][0]
+        //   edit distance is delete (i+1) chars from s to match empty t
+        v1[0] = i + 1;
+        int deletionCost = 0;
+        int insertionCost = 0;
+        int substitutionCost = 0;
+
+        // use formula to fill in the rest of the row
+        for (int j = 0; j < rows - 1; j++)
+        {
+            // calculating costs for A[i+1][j+1]
+            deletionCost = v0[j + 1] + 1;
+            insertionCost = v1[j] + 1;
+
+            if (str_l[i] == str_r[j])
+            {
+                substitutionCost = v0[j];
+            }
+            else
+            {
+                substitutionCost = v0[j] + 1;
+            }
+
+            v1[j + 1] = get_min(deletionCost, insertionCost, substitutionCost);
+
+            // copy v1 (current row) to v0 (previous row) for next iteration
+            // since data in v1 is always invalidated, a swap without copy could be more efficient
+        }
+        //swap v0 with v1
+        // after the last swap, the results of v1 are now in v0
+    }
+    return v0[rows];
+}
+
 int StringDistanceResolver::get_min(int a, int b, int c)
 {
     auto result = a < b ? a : b;
     result = result < c ? result : c;
     return result;
-}
-
-int StringDistanceResolver::get_distance_two_vec(string str_l, string str_r)
-{
-    // auto rows = str_l.length() + 1;
-    // auto cols = str_r.length() + 1;
-    // auto distance = 0;
-
-    // std::vector<int> v0 = std::vector<int>(rows);
-    // std::vector<int> v1 = std::vector<int>(cols);
-
-    // // initialize v0 (the previous row of distances)
-    // // this row is A[0][i]: edit distance for an empty s
-    // // the distance is just the number of characters to delete from t
-    // for (int i = 0; i < rows; i++)
-    // {
-    //     v0[i] = i;
-    // }
-
-    // for (int i = 0; i < cols - 1; i++)
-    // {
-    //     // calculate v1 (current row distances) from the previous row v0
-
-    //     // first element of v1 is A[i+1][0]
-    //     //   edit distance is delete (i+1) chars from s to match empty t
-    //     v1[0] = i + 1;
-    //     int deletionCost = 0;
-    //     int insertionCost = 0;
-    //     int substitutionCost = 0;
-
-    //     // use formula to fill in the rest of the row
-    //     for (int j = 0; j < rows - 1; j++)
-    //     {
-    //         // calculating costs for A[i+1][j+1]
-    //         deletionCost = v0[j + 1] + 1;
-    //         insertionCost = v1[j] + 1;
-
-    //         if (str_l[i] == str_r[j])
-    //         {
-    //             substitutionCost = v0[j];
-    //         }
-    //         else
-    //         {
-    //             substitutionCost = v0[j] + 1;
-    //         }
-
-    //         v1[j + 1] = minimum(deletionCost, insertionCost, substitutionCost);
-
-    //         // copy v1 (current row) to v0 (previous row) for next iteration
-    //         // since data in v1 is always invalidated, a swap without copy could be more efficient
-    //     }
-    //     //swap v0 with v1
-    //     // after the last swap, the results of v1 are now in v0
-    // }
-    // return v0[rows];
 }
