5b037276879afba2abb0a4b82179a87cffca41e5
Christian Lascsak <christian.lascsak7@gmail.com>
2020-12-08 12:31:13 +0100

    add matrix version again

--- src/Services/StringDistanceResolver.h
+++ src/Services/StringDistanceResolver.h
@@ -9,14 +9,16 @@ private:
 public:
     StringDistanceResolver(/* args */);
 
-    int get_leventshtein_distance(string, string);
+    int get_distance_vec(string, string);
+    int get_distance_two_vec(string, string);
+    int get_distance_matrix(string, string);
 };
 
 StringDistanceResolver::StringDistanceResolver(/* args */)
 {
 }
 
-int StringDistanceResolver::get_leventshtein_distance(string str_l, string str_r)
+int StringDistanceResolver::get_distance_vec(string str_l, string str_r)
 {
     auto rows = str_l.length() + 1;
     auto cols = str_r.length();
@@ -56,4 +58,52 @@ int StringDistanceResolver::get_leventshtein_distance(string str_l, string str_r
     }
 
     return distance;
+}
+
+int StringDistanceResolver::get_distance_matrix(string str_l, string str_r)
+{
+    auto rows = str_l.length() + 1;
+    auto cols = str_r.length() + 1;
+    auto distance = 0;
+    std::vector<std::vector<int>> matrix(str_l.length() + 1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        matrix[i].resize(cols + 1);
+    }
+
+    for (int i = 1; i < rows; i++)
+    {
+        matrix[i][0] = i;
+    }
+
+    for (int i = 1; i < cols; i++)
+    {
+        matrix[0][i] = i;
+    }
+
+    for (int i = 1; i < rows; i++)
+    {
+        for (int j = 1; j < cols; j++)
+        {
+            matrix[i][j] = matrix[i - 1][j];
+
+            if (matrix[i][j] > matrix[i - 1][j - 1])
+            {
+                matrix[i][j] = matrix[i - 1][j - 1];
+            }
+
+            if (matrix[i][j] > matrix[i][j - 1])
+            {
+                matrix[i][j] = matrix[i][j - 1];
+            }
+
+            if (str_l[i - 1] != str_r[j - 1])
+            {
+                matrix[i][j]++;
+            }
+        }
+    }
+
+    return matrix[rows - 1][cols - 1];
 }
